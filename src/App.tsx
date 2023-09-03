1756d71ce172e6369ee4ff299752358d9129e727
Hopla-tech <92076475+Hopla-tech@users.noreply.github.com>
2023-09-03 18:37:32 +0800

    Changes:     + Added useReducer() setup     + Added context setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import styles from './css/App.module.css'
+import styles from './css/App.module.css';
 
 import ButtonContainers from './components/ButtonContainers';
 import Header from './components/Header';
 import ResultField from './components/ResultField';
 
+import { CalculatorProvider } from './context/CalculatorContext';
+
 function App() {
   return (
-    <div className={styles["main-wrapper"]}>
-      <Header />
-      <ResultField />
-      <ButtonContainers />
-    </div>
+    <CalculatorProvider>
+      <div className={styles['main-wrapper']}>
+        <Header />
+        <ResultField />
+        <ButtonContainers />
+      </div>
+    </CalculatorProvider>
   );
 }
 
